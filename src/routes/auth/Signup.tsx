85654bd492a2ad9152483c326d7df15664afc79f
Aditya Naida <adityanaida@gmail.com>
2025-05-29 18:41:15 +0530

    [MODIFY] Auth Loader

--- src/routes/auth/Signup.tsx
+++ src/routes/auth/Signup.tsx
@@ -188,17 +188,15 @@ export default function Signup() {
               Signup{" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
+                width={24}
+                height={24}
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5 animate-spin"
+                className="animate-spin text-white"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                />
+                  fill="currentColor"
+                  d="M12 2.25c-5.384 0-9.75 4.366-9.75 9.75s4.366 9.75 9.75 9.75v-2.437A7.312 7.312 0 1 1 19.313 12h2.437c0-5.384-4.366-9.75-9.75-9.75"
+                ></path>
               </svg>
             </div>
           ) : (
