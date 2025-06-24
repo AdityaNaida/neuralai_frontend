d86795b323570d26212528a12b4f3e15d706bda7
Aditya Naida <adityanaida@gmail.com>
2025-06-24 15:37:17 +0530

    [ADD] 2.5 Flash Model

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -18,7 +18,8 @@ const safetySettingArray = [
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  // model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash",
   safetySettings: safetySettingArray,
 });
 
